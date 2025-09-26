ccabe33eb3992ba852f30abd5494843fe46238e0
IGOR DA SILVA REIS <igorsreis2017@gmail.com>
2025-09-26 00:33:47 -0300

    Mudança geral na estilização + criação da página de 1x1

--- scripts/solo.js
+++ scripts/solo.js
@@ -3,7 +3,6 @@ const listaChutes = [];
 const dif1 = document.getElementById("btn1a10");
 const dif2 = document.getElementById("btn1a100");
 const dif3 = document.getElementById("btn1a1000");
-
 const placarTentativas = document.getElementById("tentativas");
 const placarAcertos = document.getElementById("acertos");
 const chute = document.getElementById("inputSolo");
@@ -11,6 +10,8 @@ const btnChute = document.getElementById("btnChuteSolo");
 const txtResultado = document.getElementById("txtResultado");
 let tentativas = 0;
 let acertos = 0;
+
+const listaChutesHtml = document.querySelector('.list-tentativas');
 
 btnChute.addEventListener("click", function () {
     validarChute();
@@ -22,25 +23,25 @@ console.log('NUMERO SECRETO: ' +numeroSecreto)
 let nivelAtual = null;
 dif1.addEventListener("click", function () {
     nivelAtual = 1;
-    dif1.style.backgroundColor = 'rgba(25, 25, 120, 1)'; dif1.style.color = 'white';
-    dif2.style.backgroundColor = '';
-    dif3.style.backgroundColor = '';
+    dif1.style.background = 'linear-gradient(135deg, #200f81ff, #ff6600)';
+    dif2.style.background = '';
+    dif3.style.background = '';
     numeroSecreto = Math.floor(Math.random() * 10) + 1
     console.log("Novo número (1-10):", numeroSecreto);
 });
 dif2.addEventListener("click", function () {
     nivelAtual = 2;
-    dif1.style.backgroundColor = '';
-    dif2.style.backgroundColor = 'rgba(25, 25, 120, 1)'; dif2.style.color = 'white';
-    dif3.style.backgroundColor = '';
+    dif1.style.background = '';
+    dif2.style.background = 'linear-gradient(135deg, #200f81ff, #ff6600)';
+    dif3.style.background = '';
     numeroSecreto = Math.floor(Math.random() * 100) + 1
     console.log("Novo número (1-100):", numeroSecreto);
 });
 dif3.addEventListener("click", function () {
     nivelAtual = 3;
-    dif1.style.backgroundColor = '';
-    dif2.style.backgroundColor = '';
-    dif3.style.backgroundColor = 'rgba(25, 25, 120, 1)'; dif3.style.color = 'white';
+    dif1.style.background = '';
+    dif2.style.background = ''
+    dif3.style.background = 'linear-gradient(135deg, #200f81ff, #ff6600)';
     numeroSecreto = Math.floor(Math.random() * 1000) + 1
     console.log("Novo número (1-1000):", numeroSecreto);
 });
@@ -66,7 +67,7 @@ function validarChute(){
         txtResultado.classList.add('resultadoCorreto');
         placarAcertos.textContent = acertos;
         placarTentativas.textContent = tentativas;
-        txtResultado.textContent = 'ACERTOU O NUMERO SECRETO: '+numeroSecreto;
+        txtResultado.textContent = 'ACERTOU O NUMERO SECRETO: '+numeroSecreto+' | NOVO NÚMERO GERADO!';
         if (nivelAtual === 1) numeroSecreto = Math.floor(Math.random() * 10) + 1;
         if (nivelAtual === 2) numeroSecreto = Math.floor(Math.random() * 100) + 1;
         if (nivelAtual === 3) numeroSecreto = Math.floor(Math.random() * 1000) + 1;
@@ -84,12 +85,10 @@ function validarChute(){
                 txtResultado.textContent = 'O número secreto é menor';
                 txtResultado.classList.remove('resultadoCorreto')
             }
-        listaChutes.push(valorChute)
-        console.log(listaChutes);
-        
-    }       
-
+    }
     chute.value = "";
     chute.focus();
-}
+}       
+
+
 
